540fae31482fa6d741c0bd794c687f732698da46
LioTG <94992609+LioTG@users.noreply.github.com>
2023-11-07 14:20:22 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv/config');
+const { Client, IntentsBitField } = require('discord.js');
+const { CommandHandler } = require('djs-commander');
+const mongoose = require('mongoose');
+const path = require('path');
+const Product = require('../L-Shop Bot/schemas/Product');
+const { EmbedBuilder } = require('@discordjs/builders');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+new CommandHandler({
+    client,
+    eventsPath: path.join(__dirname, 'events'),
+    commandsPath: path.join(__dirname, 'commands'),
+});
+
+(async () => {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to database.");
+
+    client.login(process.env.TOKEN);
+})();
+
+
